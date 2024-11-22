1c523d8bc5bcca0aa1046cb8e902704863b52505
Viggo Gustafsson <viggogustafsson@gmail.com>
2024-11-22 15:13:37 +0100

    added basic models

---
+++ main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()    
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField( upload_to='photos/',blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+ 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
+    author = models.OneToOneField(UserInfo, on_delete=models.CASCADE) # would deleting user mean to deleting comment or other way around?
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
