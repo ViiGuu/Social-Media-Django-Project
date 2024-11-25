d7e39213a14215687fe681af5f6612dff13c897c
Viggo Gustafsson <viggogustafsson@gmail.com>
2024-11-25 13:06:16 +0100

    added user registration + test html

---
+++ users_app/forms.py
@@ -0,0 +1,34 @@
+from main.models import UserInfo
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+        
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            raise forms.ValidationError(list(e.messages))
+
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class UserProfileInfoForm(forms.Form):
+    class Meta:
+        model = UserInfo
+        fields = ('profile_pic')
--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from users_app.forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from main.models import UserInfo
 
 # Create your views here.
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_reg_form = UserRegistrationForm(data=request.POST)
+
+        if user_reg_form.is_valid():
+            user = User.objects.create_user(
+                username=user_reg_form.cleaned_data['username'], 
+                password=user_reg_form.cleaned_data['password'])
+            
+            #links user to a UserInfo model (so they can upload profile pics etc.)
+            UserInfo.objects.create(user=user)
+            registered = True
+        else:
+            print(user_reg_form.errors)
+        
+    else:
+        user_reg_form = UserRegistrationForm()
+
+    return render(request, 'test_registration_form.html', {
+        'user_reg_form' : user_reg_form, 
+        'registered' : registered})
