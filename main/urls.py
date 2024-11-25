b4d23a8ca338e9a1e205b99c42c39bba85655bbe
Muhammad Usman <usman820901@gmail.com>
2024-11-25 14:55:04 +0100

    layout

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from .views import *
 from django.urls import path
+from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-    
+    path('', views.all_posts, name='all_posts'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def all_posts(request):
+  
+    return render(request, 'all_posts.html')
