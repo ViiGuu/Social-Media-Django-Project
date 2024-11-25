92736f1707dc92519e95597df65124e8bf89e812
Viggo Gustafsson <viggogustafsson@gmail.com>
2024-11-25 14:44:21 +0100

    added functionality + testing html for user login

--- users_app/urls.py
+++ users_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', register, name='register')
+    path('register/', user_register, name='register'),
+    path('login/', user_login, name='login')
 ]
