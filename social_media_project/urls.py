86d2f727b334b18789c2029dd4781e80f6d67650
Ratnaprabha Kadam <ratnaprabha_k@yahoo.com>
2024-11-25 22:57:53 +0100

    Resolved conflicts

--- social_media_project/urls.py
+++ social_media_project/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('users/', include('users_app.urls')),
-    path('posts/', include('main.urls')),
+    path('posts/', include('main.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
