692bfc73e1bd81ab099588c697665b229ea8dfd8
Ratnaprabha Kadam <ratnaprabha_k@yahoo.com>
2024-11-25 23:29:32 +0100

    Fixed conflicts

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,16 +1,9 @@
-from .views import *
 from django.urls import path
-from django.contrib import admin
-#from django.conf import settings
-#from django.conf.urls.static import static
+from . import views
 
 app_name = 'profile_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/',views.profile_view, name='profile'),
-        
 ]
-
-#if settings.DEBUG:
- #   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
