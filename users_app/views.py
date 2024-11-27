00b20caf9be3a31fc66b0be2c9db036d8e78bc2c
Viggo Gustafsson <viggogustafsson@gmail.com>
2024-11-27 13:17:39 +0100

    added logout functionality

--- users_app/views.py
+++ users_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from users_app.forms import UserRegistrationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from profile_app.models import Profile
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
@@ -53,3 +54,8 @@ def user_login(request):
             return HttpResponse("Invalid login details")
     else:
         return render(request, 'login.html') #placeholder html for testing
+    
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('users:login')
