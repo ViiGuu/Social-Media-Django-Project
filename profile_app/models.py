993f68e2a5dc63c4cabd915bcf328133a630d566
Viggo Gustafsson <viggogustafsson@gmail.com>
2024-11-28 15:29:40 +0100

    having no profile picture no longer throws errors, and correctly shows default user.png

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,6 +8,14 @@ class Profile(models.Model):
     profile_pic = models.ImageField(default = 'profile_images/user.png', upload_to='profile_images/',blank=True, null=True)
     gender = models.CharField(choices=[('M', 'Male'), ('F', 'Female')], default='M', max_length=1)
     age = models.IntegerField(default=None)
+
+    @property
+    def profile_pic_url(self):
+        if self.profile_pic:
+            return self.profile_pic.url
+        else:
+            return f'{settings.MEDIA_URL}profile_images/user.png'
+            
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
