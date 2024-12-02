cd1e383e27bc370a77fcdac2033ead941479f60a
Muhammad Usman <usman820901@gmail.com>
2024-12-02 14:30:59 +0100

    new_post-update-post

--- posts_app/forms.py
+++ posts_app/forms.py
@@ -5,8 +5,10 @@ from main.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = [
-            'title',
-            'body',
-            'image'
-        ]
+        fields = ['title','body','image']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter post title'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter post content'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
