f8af2530dcd741f416fac1c08887d33c26be4f68
yam91 <yamitalon12@gmail.com>
2024-12-03 10:39:12 +0100

    changed post page url again

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -5,7 +5,7 @@ app_name = 'posts_app'
 
 urlpatterns = [
     path('', views.all_posts, name='all_posts'),
-    path('<slug:slug>', views.post_page, name = "page"),
+    path('post/<slug:slug>', views.post_page, name = "page"),
     path('my_posts', views.my_posts, name = 'my_posts'),
     path('my_posts/<int:post_id>', views.delete_post, name = 'delete_post'),
     path('new_post', views.new_post, name='new_post'),
