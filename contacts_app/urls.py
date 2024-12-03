a3e9779f1403dd869034302f650ea4dcc99e4903
Iralim <irik.alimbaev@gmail.com>
2024-12-03 11:41:36 +0100

    Organised cod

--- contacts_app/urls.py
+++ contacts_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'contacts_app'
 
 urlpatterns = [
-    path('show_profile/<int:id>', views.show_profile, name='show_profile'),
-    path('show_friends', views.show_friends, name='show_friends'),
+    path('view_profile/<int:user_id>', views.view_profile, name='view_profile'),
+    path('view_friends', views.view_friends, name='view_friends'),
     path('send_request/<int:user_id>/', views.send_request, name='send_request'),
-    path('accept_request/<int:request_id>/', views.accept_request_view, name='accept_request'),
-    path('reject_request/<int:request_id>/', views.reject_request_view, name='reject_request'),
+    path('accept_request/<int:request_id>/', views.accept_request, name='accept_request'),
+    path('reject_request/<int:request_id>/', views.reject_request, name='reject_request'),
     path('cancel_friend_request/<int:to_user_id>/', views.cancel_friend_request, name='cancel_friend_request'),
     path('remove_friend/<int:friend_id>/', views.remove_friend, name='remove_friend'),
 ]
--- contacts_app/views.py
+++ contacts_app/views.py
@@ -1,52 +1,79 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .services import send_friend_request, accept_friend_request, reject_friend_request
-from django.contrib.auth.models import User
-from main.models import Post
-from profile_app.models import Profile
-from .services import get_friends
-from .models import Friendship, FriendshipRequest
 from django.db.models import Q
+from django.contrib.auth.models import User
+
+from .services import send_friend_request, accept_friend_request, reject_friend_request
+from .models import Friendship, FriendshipRequest
+
 
 # Create your views here.
+@login_required
+def view_profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    profile = user.profile
+
+    is_friend = FriendshipRequest.are_friends(request.user, user)
+
+    if request.user == user:
+        return redirect('profile_app:profile')
+
+    friendship_status = FriendshipRequest.friendship_status(request.user, user)
+
+    return render(
+        request,
+        'contacts_app/view_profile.html',
+        {
+            'profile': profile,
+            'friendship_status': friendship_status,
+            'is_friend': is_friend,
+        }
+    )
+
 
 @login_required
-def show_friends(request):
+def view_friends(request):
     user = request.user
     friends = user.profile.friends
     received_friend_requests = user.received_friend_requests.all()
-    return render(request, 'contacts_app/friends.html', {'friends': friends, 'received_friend_requests':received_friend_requests})
 
-@login_required
-def show_profile(request, id):
-    user = User.objects.get(id=id)
-    is_friend = FriendshipRequest.are_friends(request.user, user)
-    if request.user == user:
-        return redirect('profile_app:profile')
-    friendship_status = FriendshipRequest.friendship_status(request.user, user)
-    
-    print(friendship_status)
-    profile = user.profile
-    return render(request, 'contacts_app/show_profile.html', {'profile': profile, 'friendship_status':friendship_status, 'is_friend': is_friend})
+    return render(
+        request,
+        'contacts_app/view_friends.html',
+        {
+            'friends': friends,
+            'received_friend_requests': received_friend_requests,
+        }
+    )
+
 
 @login_required
 def send_request(request, user_id):
-    to_user = User.objects.get(id=user_id)
-    profile = to_user.profile
     to_user = get_object_or_404(User, id=user_id)
+    profile = to_user.profile
     message = send_friend_request(request.user, to_user)
     friendship_status = FriendshipRequest.friendship_status(request.user, to_user)
-    return render(request, 'contacts_app/show_profile.html', {'profile': profile, 'friendship_status': friendship_status})  
+
+    return render(
+        request,
+        'contacts_app/view_profile.html',
+        {
+            'profile': profile,
+            'friendship_status': friendship_status
+        }
+    )
+
 
 @login_required
-def accept_request_view(request, request_id):
+def accept_request(request, request_id):
     message = accept_friend_request(request_id)
     messages.info(request, message)
-    return redirect('contacts_app:show_friends')
+    return redirect('contacts_app:view_friends')
+
 
 @login_required
-def reject_request_view(request, request_id):
+def reject_request(request, request_id):
     message = reject_friend_request(request_id)
     messages.info(request, message)
     return redirect('profile_page')
@@ -56,14 +83,13 @@ def cancel_friend_request(request, to_user_id):
     to_user = User.objects.get(id=to_user_id)
     profile = to_user.profile
     
-
     try:
         to_user = User.objects.get(id=to_user_id)
         friend_request = FriendshipRequest.objects.get(from_user=request.user, to_user=to_user, status='pending')
         friend_request.delete()
         friendship_status = FriendshipRequest.friendship_status(request.user, to_user)
 
-        return render(request, 'contacts_app/show_profile.html', {'profile': profile, 'friendship_status': friendship_status})  
+        return render(request, 'contacts_app/view_profile.html', {'profile': profile, 'friendship_status': friendship_status})  
     
     except FriendshipRequest.DoesNotExist:
         return HttpResponse("The friend request was not found or has already been accepted/rejected.")
@@ -72,18 +98,15 @@ def cancel_friend_request(request, to_user_id):
 def remove_friend(request, friend_id):
     
     try:
-        # Get the user who needs to be removed from friends
         friend_to_remove = User.objects.get(id=friend_id)
-        # Find the friendship record between the current user and the selected user
         friendship = Friendship.objects.get(
             Q(user1=request.user, user2=friend_to_remove) | Q(user1=friend_to_remove, user2=request.user)
         )
         
-        # Delete the friendship record
         friendship.delete()
         FriendshipRequest.delete_all_friendship_requests(request.user, friend_to_remove)
 
-        return redirect('contacts_app:show_profile', id=friend_id)
+        return redirect('contacts_app:view_profile', user_id=friend_id)
     
     except Friendship.DoesNotExist:
         return HttpResponse("Friendship record not found.")
